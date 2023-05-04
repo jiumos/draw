04be6e2f15fedec5b9de4cc814d1eb39d92cc3e3
moleyi <1820033249@qq.com>
2023-05-04 10:20:47 +0800

    add kube_init.sh

---
+++ kube_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kubeadm init \
+--apiserver-advertise-address=192.168.163.100 \
+--kubernetes-version v1.20.15 \
+--service-cidr=10.96.0.0/12 \
+--pod-network-cidr=10.244.0.0/16
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null
