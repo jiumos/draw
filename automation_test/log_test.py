dfa0c85159f8201a8d1f589e4aa684ece2d777bf
moleyi <moleyi@yunshan.net>
2023-11-22 07:08:50 +0000

    reset repo

--- automation_test/log_test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-for i in range(10):
-
-    print(f"test{i*3}")
-    time.sleep(3)
-
-
-
--- kube_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubeadm init \
---apiserver-advertise-address=$1 \
---kubernetes-version v1.20.15 \
---service-cidr=10.96.0.0/12 \
---pod-network-cidr=10.244.0.0/16
-export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null
